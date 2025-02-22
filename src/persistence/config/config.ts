421a227f20e46e599d17ee93782233ee45b5b4af
Matias Laime <matulai1221@gmail.com>
2025-02-22 00:26:17 -0300

    Refactor: config changes for typeorm

--- src/persistence/config/config.ts
+++ src/persistence/config/config.ts
@@ -1,8 +1,14 @@
-import type { PoolOptions } from "mysql2/promise";
+import User from "@/model/user.js";
+import type { DataSourceOptions } from "typeorm";
 
-export const config: PoolOptions = {
+export const config: DataSourceOptions = {
+  type: "mysql",
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  port: 3306,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  synchronize: true,
+  logging: true,
+  entities: [User], // Aquí defines los modelos (entidades)
 };
