3a1d0a7735414eebf785183841535b5071bf96d4
Matias Laime <matulai1221@gmail.com>
2025-02-22 14:36:54 -0300

    Refactor: File name change dataBase -> dataSource

--- src/persistence/dataSource.ts
+++ src/persistence/dataSource.ts
@@ -1,4 +1,4 @@
 import { config } from "@/persistence/config/config.js";
 import { DataSource } from "typeorm";
 
-export const dataBase: DataSource = new DataSource(config);
+export const dataSourceI: DataSource = new DataSource(config);
