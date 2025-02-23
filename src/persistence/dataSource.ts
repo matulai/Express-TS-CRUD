97c3f41ab780a1cd0c6b88a7d84209366360fa9f
Matias Laime <matulai1221@gmail.com>
2025-02-23 02:09:51 -0300

    Add: Forgot to initialize dataSource

--- src/persistence/dataSource.ts
+++ src/persistence/dataSource.ts
@@ -2,3 +2,10 @@ import { config } from "@/persistence/config/config.js";
 import { DataSource } from "typeorm";
 
 export const dataSourceI: DataSource = new DataSource(config);
+
+dataSourceI
+  .initialize()
+  .then(() => {
+    // here you can start to work with your database
+  })
+  .catch(error => console.log(error));
