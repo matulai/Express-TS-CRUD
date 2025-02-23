e7a2eb9eccf8ab909eb12090ebb41ace940d1c9b
Matias Laime <matulai1221@gmail.com>
2025-02-23 02:18:26 -0300

    Add: class UserController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,21 @@
 import type { Request, Response } from "express";
+import User from "@/model/user.js";
+import UserService from "@/service/userService.js";
 
 export default class UserController {
-  createUser(_req: Request, res: Response): void {
-    res.send({ name: "Matias" });
+  private readonly userService: UserService;
+
+  constructor() {
+    this.userService = new UserService();
+  }
+
+  async createUser(req: Request, res: Response): Promise<void> {
+    try {
+      const user: User = await this.userService.createUser(req.body);
+
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(409).json({ message: error });
+    }
   }
 }
