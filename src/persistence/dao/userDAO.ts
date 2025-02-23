94d0f93b09f0930fdece7b7a9ce1a7fd49e38641
Matias Laime <matulai1221@gmail.com>
2025-02-23 02:18:07 -0300

    Add: class UserDAO

--- src/persistence/dao/userDAO.ts
+++ src/persistence/dao/userDAO.ts
@@ -1,30 +1,10 @@
-import { Repository } from "typeorm";
 import { DataSource } from "typeorm";
 import { dataSourceI } from "@/persistence/dataSource.js";
+import BaseDAO from "@/persistence/dao/baseDAO.js";
 import User from "@/model/user.js";
 
-export default class UserDAO {
-  private readonly dataSource: DataSource;
-  private readonly repository: Repository<User>;
-
+export default class UserDAO extends BaseDAO<User> {
   constructor(newDataSourceI: DataSource = dataSourceI) {
-    this.dataSource = newDataSourceI;
-    this.repository = this.dataSource.getRepository(User);
-  }
-
-  createOrUpdateUser(user: User): Promise<User> {
-    return this.repository.save(user);
-  }
-
-  removeUser(user: User): Promise<User> {
-    return this.repository.remove(user);
-  }
-
-  getUserById(id: number): Promise<User> {
-    return this.repository.findOneByOrFail({ id: id });
-  }
-
-  getAllUser(): Promise<User[]> {
-    return this.repository.find();
+    super(newDataSourceI, User);
   }
 }
