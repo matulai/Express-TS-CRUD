5b90ec6856e3a521ea5ae67f26381ae78d6f4283
Matias Laime <matulai1221@gmail.com>
2025-02-23 02:17:47 -0300

    Add: abstract class for CRUD queries

---
+++ src/persistence/dao/baseDAO.ts
@@ -0,0 +1,29 @@
+import { Repository, DataSource } from "typeorm";
+
+import type { ObjectLiteral } from "typeorm";
+/*
+ * Basic queries for DAOs like save, remove, findById, etc.
+ */
+export default abstract class BaseDAO<T extends ObjectLiteral> {
+  protected readonly repository: Repository<T>;
+
+  constructor(dataSource: DataSource, entity: { new (...args: any[]): T }) {
+    this.repository = dataSource.getRepository(entity);
+  }
+
+  async save(entity: T): Promise<T> {
+    return await this.repository.save(entity);
+  }
+
+  async remove(entity: T): Promise<T> {
+    return await this.repository.remove(entity);
+  }
+
+  async findById(id: number): Promise<T> {
+    return await this.repository.findOneByOrFail({ id } as any);
+  }
+
+  async findAll(): Promise<T[]> {
+    return await this.repository.find();
+  }
+}
